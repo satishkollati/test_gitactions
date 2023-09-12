8a4c86551a5a56e9177f81005c1891b29e9d7407
satish.kollati <satish.kollati@NE.com>
2023-09-12 14:58:45 +0530

    v5

--- src/main/java/com/example/test_git_actions/UserController.java
+++ src/main/java/com/example/test_git_actions/UserController.java
@@ -1,8 +1,7 @@
 @RestController
 public class UserController {
     @GetMapping
-   public String test_git_actions() {
-    return "hello world";
-
+    public String test_git_actions() {
+      return "hello world";
    } 
 }
